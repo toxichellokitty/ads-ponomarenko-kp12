8030af455a3ff9759d8e48f1263824ceef2b85eb
toxichellokitty <vladislavaponomarenko@gmail.com>
2021-10-20 22:29:15 +0300

    First commit

---
+++ лаб1/ads_lab1_task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace ads_lab1_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, a, b, c;
+
+            Write("Enter x: "); x = Convert.ToDouble(ReadLine());
+            Write("Enter y: "); y = Convert.ToDouble(ReadLine());
+            Write("Enter z: "); z = Convert.ToDouble(ReadLine());
+
+            c = Pow(x, z) - Cbrt(Pow(x, 2) - y * Pow(z, 3));
+
+            if (!(c == 0))
+            {
+                a = (x + y - z) / c;
+                if (!(a == 0))
+                {
+                    b = Cos((x * y + Pow(y, 2)) / Pow(a, 2));
+                    WriteLine($"a = {a}");
+                    WriteLine($"b = {b}");
+                }
+                else
+                {
+                    WriteLine($"a = {a}");
+                    WriteLine("Example b can't be counted.");
+                }
+            }
+
+            else
+                WriteLine("There are some mistakes in entered data.");
+           
+
+            ReadKey();
+        }
+    }
+}
---
+++ лаб1/ads_lab1_task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace ads_lab1_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, n1;
+            int t, k, l, k1;
+
+            Write("Enter n: "); n = Convert.ToInt32(ReadLine());
+
+            if (n > 0)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    n1 = i;
+                    k = 0;
+                    l = 0;
+
+                    while (n1 >= 1)
+                    {
+                        t = n1 % 10;
+                        k = 10 * k + t;
+                        n1 = n1 / 10;
+                    }
+
+                    if (k == i)
+                    {
+                        
+                        k = k1 = k * k;
+
+                        while (k1 >= 1)
+                        {
+                            t = k1 % 10;
+                            l = 10 * l + t;
+                            k1 = k1 / 10;
+                        }
+
+                        if (l == k)
+                        {
+                            WriteLine("Number-palindrome, which squared also gives palindrome: " + i);
+                        }
+                    }
+                }
+            }
+
+            else WriteLine("Entered number is incorrect.");
+            
+
+          ReadKey();
+        }
+    }
+}
